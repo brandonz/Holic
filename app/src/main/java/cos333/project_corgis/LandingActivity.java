6fe3c9dab6a919a29f04a6868bfe9d154b4f403e
emzhang314 <emzhang314@gmail.com>
2016-04-07 20:21:35 -0400

    add a settings page from the landing.

--- app/src/main/java/cos333/project_corgis/LandingActivity.java
+++ app/src/main/java/cos333/project_corgis/LandingActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class LandingActivity extends AppCompatActivity {
@@ -31,6 +33,36 @@ public class LandingActivity extends AppCompatActivity {
     // from the server instead of the other page.
     public void startDrinkActivity(View view) {
         // Intent intent = new Intent(this);
+    }
+
+    // TODO: Open the settings activity.
+    public void openSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_landing, menu);
+        return true;
+    }
+
+    /**
+     * Handles menu selection.
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
