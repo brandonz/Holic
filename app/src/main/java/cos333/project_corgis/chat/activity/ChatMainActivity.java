c0662ddfc97ce19010a1e8dd6f264172de24e157
jenniqpenni <jqpeng1995@gmail.com>
2016-05-03 02:49:46 -0400

    makes chat pretty

--- app/src/main/java/cos333/project_corgis/chat/activity/ChatMainActivity.java
+++ app/src/main/java/cos333/project_corgis/chat/activity/ChatMainActivity.java
@@ -212,6 +212,7 @@ public class ChatMainActivity extends AppCompatActivity {
 //        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setBackgroundTintList(getResources().getColorStateList(R.color.mainPurple));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
